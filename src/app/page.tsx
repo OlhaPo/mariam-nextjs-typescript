ec421f35832ea085f884c8685a55af19dcad52b2
Olya <ponomaryova.olya.y@gmail.com>
2023-09-27 14:23:55 +0200

    Create separate component for Contacts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Fredoka } from "next/font/google";
-import Link from "next/link";
 import Collections from "@/components/Collections";
-import { FaInstagram, FaFacebookF, FaTelegramPlane } from "react-icons/fa";
+import Contacts from "@/components/Contacts";
 
 export const fredoka = Fredoka({ subsets: ["latin"], weight: ["300", "400"] });
 
@@ -17,57 +16,7 @@ export default function Home() {
         </h1>
       </div>
       <Collections />
-      <div id="contact" className="contacts">
-        <div className="contacts-section basis-full lg:basis-1/2 lg:self-center">
-          <p>
-            <span
-              className={`${fredoka.className} font-medium text-lg md:text-2xl text-primary`}
-            >
-              mariam crochet jewelry
-            </span>{" "}
-            is a Ukrainian clothing and crocheted jewelry brand founded by
-            designer Maryna Kambur. Handcrafted lovingly in Kyiv, Ukraine.
-          </p>
-        </div>
-        <div className="contacts-section">
-          <span className="contacts-subheader">Menu</span>
-          <Link href={"/about"}>About</Link>
-          <Link href={"/shop"}>Shop</Link>
-          <Link href={"/orders-info"}>Orders and delivery</Link>
-        </div>
-        <div className="contacts-section">
-          <span className="contacts-subheader">Contacts</span>
-          <div className="flex flex-row gap-4">
-            <a
-              href="https://www.instagram.com/mariam_crochet_jewelry/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaInstagram size={20} />
-            </a>
-            <a
-              href="https://t.me/+380678354246"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaTelegramPlane size={20} />
-            </a>
-            <a
-              href="https://www.facebook.com/mariam.corner/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaFacebookF size={20} />
-            </a>
-          </div>
-          <div className="flex flex-col gap-1 md:gap-4">
-            <a href="tel:+380678354246">+380678354246</a>
-            <a href="mailto:mariam_jewelry@gmail.com">
-              mariam_jewelry@gmail.com
-            </a>
-          </div>
-        </div>
-      </div>
+      <Contacts />
     </>
   );
 }
---
+++ src/components/Contacts.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import { FaInstagram, FaFacebookF, FaTelegramPlane } from "react-icons/fa";
+import { fredoka } from "@/app/page";
+
+export default function Contacts() {
+  return (
+    <footer id="contacts" className="contacts">
+      <div className="contacts-section basis-full lg:basis-1/2 lg:self-center">
+        <p>
+          <span
+            className={`${fredoka.className} font-medium text-lg md:text-2xl text-primary`}
+          >
+            mariam crochet jewelry
+          </span>{" "}
+          is a Ukrainian clothing and crocheted jewelry brand founded by
+          designer Maryna Kambur. Handcrafted lovingly in Kyiv, Ukraine.
+        </p>
+      </div>
+      <div className="contacts-section">
+        <span className="contacts-subheader">Menu</span>
+        <Link href={"/about"}>About</Link>
+        <Link href={"/shop"}>Shop</Link>
+        <Link href={"/orders-info"}>Orders and delivery</Link>
+      </div>
+      <div className="contacts-section">
+        <span className="contacts-subheader">Contacts</span>
+        <div className="flex flex-row gap-4">
+          <a
+            href="https://www.instagram.com/mariam_crochet_jewelry/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram size={20} />
+          </a>
+          <a
+            href="https://t.me/+380678354246"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTelegramPlane size={20} />
+          </a>
+          <a
+            href="https://www.facebook.com/mariam.corner/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebookF size={20} />
+          </a>
+        </div>
+        <div className="flex flex-col gap-1 md:gap-4">
+          <a href="tel:+380678354246">+380678354246</a>
+          <a href="mailto:mariam_jewelry@gmail.com">mariam_jewelry@gmail.com</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
