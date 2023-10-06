f17966ebcbc6fd83be789f5b6c1bd93485114b48
Olya <ponomaryova.olya.y@gmail.com>
2023-10-06 12:17:08 +0200

    Update layout for admin page

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,5 +1,11 @@
-const AdminLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="bg-red-500">{children}</div>;
-};
-
-export default AdminLayout;
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen mx-auto w-full px-4 pt-8">
+      <main className="my-0 py-16">{children}</main>
+    </div>
+  );
+}
