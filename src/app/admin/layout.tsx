baac20e09e99256a69078b0075125e00468576b2
Olya <ponomaryova.olya.y@gmail.com>
2023-10-06 13:58:44 +0200

    Add styling to login page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,9 +3,5 @@ export default function AdminLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="flex flex-col items-center min-h-screen w-full px-4 pt-8 bg-red-300">
-      <div className="my-0 py-16">{children}</div>
-    </div>
-  );
+  return <div>{children}</div>;
 }
--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,7 +1,9 @@
 export default function Login() {
   return (
-    <div>
-      <h2>Login to admin panel</h2>
+    <div className="min-h-screen flex items-center">
+      <div className="text-center w-full">
+        <h2>Login to admin panel</h2>
+      </div>
     </div>
   );
 }
