fc2c7031f0312634f89390f08e590fdbd89b0f3a
Olya <ponomaryova.olya.y@gmail.com>
2023-10-08 20:38:40 +0200

    Refactor code on item pg: add condition statement

--- src/app/(user)/items/page.tsx
+++ src/app/(user)/items/page.tsx
@@ -8,17 +8,22 @@ import { useSearchParams } from "next/navigation";
 export default function Items() {
   const allProducts = [...productsInStock, ...productsToPreOrder];
   const searchParams = useSearchParams();
+  const activeCategory = searchParams.get("category");
+  let filteredProducts = allProducts;
+  if (activeCategory) {
+    filteredProducts = filteredProducts.filter(
+      (p) => p.category === activeCategory
+    );
+  }
 
   return (
     <div className="section-container">
       <div className="flex flex-col flex-wrap gap-12">
-        {allProducts
-          .filter((p) => p.category === searchParams.get("category"))
-          .map((product) => (
-            <div key={product.title}>
-              <ProductItem key={product.id} product={product} />
-            </div>
-          ))}
+        {filteredProducts.map((product) => (
+          <div key={product.title}>
+            <ProductItem key={product.id} product={product} />
+          </div>
+        ))}
       </div>
     </div>
   );
