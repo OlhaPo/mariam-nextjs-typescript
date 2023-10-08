144ed02954dc2c56d0d499124eed17868f723001
Olya <ponomaryova.olya.y@gmail.com>
2023-10-08 14:56:52 +0200

    Create folder for collection constants

--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { mainPageCollections } from "@/constants/collectionCoverImage";
+import { mainPageCollections } from "@/constants/collection/constants";
 
 export function Collections() {
   return (
