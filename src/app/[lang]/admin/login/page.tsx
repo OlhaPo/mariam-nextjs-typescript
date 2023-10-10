ee3587a963bc2a5c7d0e5777487a67fc4af7816b
Olya <ponomaryova.olya.y@gmail.com>
2023-10-10 18:51:48 +0200

    Add structure for admin login form

--- src/app/[lang]/admin/login/page.tsx
+++ src/app/[lang]/admin/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import LinkButton from "@/components/uikit/button";
 import { Formik, Field, Form, FormikHelpers } from "formik";
 
 interface Values {
@@ -13,7 +12,7 @@ export default function Login() {
     <div className="min-h-screen flex items-center">
       <div className="text-center w-full">
         <h2>Welcome to admin panel</h2>
-        <div>
+        <div className="mt-10">
           <Formik
             initialValues={{
               username: "",
@@ -29,12 +28,21 @@ export default function Login() {
               }, 500);
             }}
           >
-            <Form className="">
-              <label htmlFor="login">Username</label>
+            <Form className="flex flex-col gap-4 w-1/4 mx-auto">
+              <label htmlFor="login" className="text-left">
+                Username
+              </label>
               <Field id="username" name="username" />
-              <label htmlFor="password">Password</label>
+              <label htmlFor="password" className="text-left">
+                Password
+              </label>
               <Field id="password" name="password" />
-              <button type="submit">Login</button>
+              <button
+                type="submit"
+                className="rounded-md border-2 p-2 bg-white text-black"
+              >
+                Login
+              </button>
             </Form>
           </Formik>
         </div>
--- src/components/uikit/button/index.tsx
+++ src/components/uikit/button/index.tsx
@@ -3,9 +3,24 @@ import { ReactNode } from "react";
 import Link from "next/link";
 import clsx from "clsx";
 
+type LinkButtonProps = React.HTMLAttributes<HTMLButtonElement>
+
+export default function LinkButton = (props: LinkButtonProps) => {
+  return (
+    <button
+      {...props}
+      href={props.href}
+      className={clsx("btn-nav", props.className)}
+    >
+      {props.children}
+    </button>
+  );
+};
+
 interface LinkButtonProps extends LinkProps {
   children: ReactNode;
   className?: string;
+  
 }
 export default function LinkButton(props: LinkButtonProps) {
   return (
