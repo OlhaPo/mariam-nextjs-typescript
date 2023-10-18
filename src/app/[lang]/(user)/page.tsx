43a02ed4e7bc2be8608036fec89c6ba6c1b1e467
Olya <ponomaryova.olya.y@gmail.com>
2023-10-18 21:06:23 +0200

    Change position of main page header

--- src/app/[lang]/(user)/page.tsx
+++ src/app/[lang]/(user)/page.tsx
@@ -5,7 +5,7 @@ export default function Home({ params }: { params: { lang: Locale } }) {
   return (
     <>
       <div className="main">
-        <h1 className="font-fredoka font-light text-4xl md:text-5xl">
+        <h1 className="font-fredoka font-light text-4xl md:text-5xl relative -top-10 md:top-0">
           <span className="text-center block md:inline mb-1 md:mb-0 ">
             mariam{" "}
           </span>
