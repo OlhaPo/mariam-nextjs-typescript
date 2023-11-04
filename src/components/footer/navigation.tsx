2189c8a918492656f51cae919569424584e11423
Olya <ponomaryova.olya.y@gmail.com>
2023-11-04 19:51:25 +0100

    Fix broken link in footer

--- src/components/footer/navigation.tsx
+++ src/components/footer/navigation.tsx
@@ -8,7 +8,7 @@ export async function Navigation({ lang }: { lang: Locale }) {
     <div className="contacts-section">
       <span className="contacts-subheader">{footer.menu_subtitle}</span>
       <NavLink href={`/${lang}/about`}>{navigation.about}</NavLink>
-      <NavLink href={`/${lang}/collections`}>{navigation.collections}</NavLink>
+      <NavLink href={`/${lang}/#collections`}>{navigation.collections}</NavLink>
       <NavLink href={`/${lang}/shop`}>{navigation.shop}</NavLink>
       {/* <NavLink href={`/${lang}/orders`}>{navigation.orders}</NavLink> */}
     </div>
