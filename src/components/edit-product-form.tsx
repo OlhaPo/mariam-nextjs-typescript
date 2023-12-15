bbdb38b6749926c309985063106eedc161831e6c
Olya <ponomaryova.olya.y@gmail.com>
2023-12-15 12:45:25 +0100

    Fix bug in importing new IMG URL field

--- src/components/edit-product-form.tsx
+++ src/components/edit-product-form.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import * as Form from "@radix-ui/react-form";
 import { ProductItem } from "@/models/ProductSchema";
 import ImportImageUrl from "./import-image-url";
+import { IoMdAdd } from "react-icons/io";
 
 interface ProductFormProps {
   data?: ProductItem;
@@ -15,6 +16,7 @@ export default function EditProductForm({ data, onSave }: ProductFormProps) {
   const [titleEn, setTitleEn] = useState("");
   const [price, setPrice] = useState(0);
   const [imageUrls, setImageUrls] = useState([""]);
+  const [imageUrl, setImageUrl] = useState("");
 
   useEffect(() => {
     if (!data) return;
@@ -35,9 +37,26 @@ export default function EditProductForm({ data, onSave }: ProductFormProps) {
     });
   }
 
-  // function addGoogleDriveImageUrl(newUrls: string) {
-  //   setImageUrls(newUrls);
-  // }
+  function handleImageUrlChange(
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) {
+    const value = e.target.value;
+    const urls = [...imageUrls];
+    urls[index] = value;
+    setImageUrls(urls);
+  }
+
+  function addGoogleDriveImageUrl(newUrl: string, index: number) {
+    // Update the corresponding URL in the array
+    setImageUrls((prevUrls) =>
+      prevUrls.map((prevUrl, i) => (i === index ? newUrl : prevUrl))
+    );
+  }
+
+  function handleAddingImageUrl() {
+    setImageUrls([...imageUrls, imageUrl]);
+  }
 
   return (
     <div>
@@ -86,7 +105,7 @@ export default function EditProductForm({ data, onSave }: ProductFormProps) {
             />
           </Form.Control>
         </Form.Field>
-        <Form.Field className="grid mb-[10px]" name="collectionName">
+        <Form.Field className="grid mb-[10px]" name="price">
           <div className="flex items-baseline justify-between">
             <Form.Label className="text-[15px] font-medium leading-[35px]">
               Price in UAH
@@ -108,32 +127,43 @@ export default function EditProductForm({ data, onSave }: ProductFormProps) {
             />
           </Form.Control>
         </Form.Field>
-        {/* <Form.Field className="mb-[10px]" name="collectionImage">
-          <div className="flex items-baseline justify-between">
-            <Form.Label className="text-[15px] font-medium leading-[35px]">
-             Images<span className="icon-required">*</span>
-            </Form.Label>
-
-            <Form.Message
-              className="text-[13px] opacity-[0.8]"
-              match="valueMissing"
-            >
-              The field can not be blank.
-            </Form.Message>
-          </div>
-          <div className="flex flex-wrap gap-4">
-            <Form.Control asChild>
-              <input
-                className="box-border w-[80%] h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
-                type="text"
-                required
-                value={imageUrls}
-                onChange={(e) => setImageUrls(e.target.value)}
+        {imageUrls.map((singleImage, index) => (
+          <div key={index}>
+            <Form.Field className="mb-[10px]" name={`productImage-${index}`}>
+              <div className="flex items-baseline justify-between">
+                <Form.Label className="text-[15px] font-medium leading-[35px]">
+                  Images<span className="icon-required">*</span>
+                </Form.Label>
+                <Form.Message
+                  className="text-[13px] opacity-[0.8]"
+                  match="valueMissing"
+                >
+                  The field can not be blank.
+                </Form.Message>
+              </div>
+              <div className="flex flex-wrap gap-4">
+                <Form.Control asChild>
+                  <input
+                    className="box-border w-[80%] h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
+                    type="text"
+                    required
+                    value={singleImage}
+                    onChange={(e) => handleImageUrlChange(e, index)}
+                  />
+                </Form.Control>
+              </div>
+              <ImportImageUrl
+                onAdd={(newUrl) => addGoogleDriveImageUrl(newUrl, index)}
               />
-            </Form.Control>
-            <ImportImageUrl onAdd={addGoogleDriveImageUrl} />
+            </Form.Field>{" "}
           </div>
-        </Form.Field> */}
+        ))}
+        <button
+          onClick={() => handleAddingImageUrl()}
+          className="inline-flex gap-3"
+        >
+          <IoMdAdd /> Add more images
+        </button>
         <Form.Submit className="box-border w-full inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[10px]">
           Save
         </Form.Submit>
