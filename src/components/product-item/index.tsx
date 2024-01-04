6a2cfc5015c0b94269c4c86cd630ba09949f6a0e
Olya <ponomaryova.olya.y@gmail.com>
2024-01-04 16:33:58 +0100

    Add button "Buy/Pre-order". Disable button for sold out items.

--- src/components/product-item/index.tsx
+++ src/components/product-item/index.tsx
@@ -2,14 +2,17 @@ import { ProductItem } from "@/models/ProductSchema";
 import Image from "next/image";
 import { Locale } from "../../../i18n.config";
 import { getLangField } from "@/lib/dictionaryUtils";
+import LinkButton from "../uikit/button";
+import { getDictionary } from "@/lib/dictionary";
 
-export default function ProductItem({
+export default async function ProductItem({
   product,
   lang,
 }: {
   product: ProductItem;
   lang: Locale;
 }) {
+  const { cartButton, productStatus } = await getDictionary(lang);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-12">
       <div>
@@ -22,8 +25,14 @@ export default function ProductItem({
       </div>
       <div className="flex flex-col gap-8">
         <h2 className="text-left">{getLangField(product, "title_", lang)}</h2>
-        <span>{product.price} UAH</span>
+        <p>{(productStatus as any)[product.status.toString()]}</p>
+        <p>{product.price} UAH</p>
         <p>{getLangField(product, "description_", lang)}</p>
+        {product.status !== 2 ? (
+          <LinkButton href={`/${lang}/cart`}>
+            {(cartButton as any)[product.status.toString()]}
+          </LinkButton>
+        ) : null}
       </div>
     </div>
   );
