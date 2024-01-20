415282939af66db023103abc2f69bff3ed3925eb
Olya <ponomaryova.olya.y@gmail.com>
2024-01-20 20:05:42 +0100

    Fix error

--- src/app/[lang]/(user)/order-confirmation/page.tsx
+++ src/app/[lang]/(user)/order-confirmation/page.tsx
@@ -2,7 +2,8 @@ export default function OrderConfirmationPage() {
   return (
     <div>
       <p>
-        Thank you for your order! We'll reach out to you as soon as possible.
+        Thank you for your order! We&apos;ll reach out to you as soon as
+        possible.
       </p>
     </div>
   );
