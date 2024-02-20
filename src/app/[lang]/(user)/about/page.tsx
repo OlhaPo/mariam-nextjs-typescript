f8a41145b548b9a7b3175cfbce0ac308695613ba
Olya <ponomaryova.olya.y@gmail.com>
2024-02-20 11:57:26 +0100

    Change content on about page.

--- src/app/[lang]/(user)/about/page.tsx
+++ src/app/[lang]/(user)/about/page.tsx
@@ -12,7 +12,7 @@ export default async function About({
   const { page } = await getDictionary(lang);
   return (
     <section className="max-w-[1040px] m-auto py-4 md:py-16 px-6 lg:px-0">
-      <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-5 md:gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 items-start gap-5 md:gap-8">
         <div>
           <Image
             src={HeroImage}
@@ -25,7 +25,11 @@ export default async function About({
         <div>
           <div>
             <h2 className="text-left">{page.about.title}</h2>
-            <p dangerouslySetInnerHTML={{ __html: page.about.description }}></p>
+            <p
+              dangerouslySetInnerHTML={{ __html: page.about.description_p_one }}
+            ></p>
+            <p>{page.about.description_p_two}</p>
+            <p>{page.about.description_p_three}</p>
           </div>
           <div>
             <p>{page.about.shop_description}</p>
@@ -43,6 +47,7 @@ export default async function About({
               </LinkButton>
             </div>
           </div>
+          <p>{page.about.description_ending}</p>
         </div>
       </div>
     </section>
