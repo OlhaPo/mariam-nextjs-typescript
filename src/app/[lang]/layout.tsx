02a202ced8431459668819956c8d1be804248e4a
Olya <ponomaryova.olya.y@gmail.com>
2024-05-21 11:48:27 +0200

    add keywords and robots to metadata description

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -19,6 +19,28 @@ export const metadata: Metadata = {
   title: "mariam crochet jewelry - Maryna Kambur",
   description:
     "mariam crochet jewelry is a Ukrainian clothing and crocheted jewelry brand founded by designer Maryna Kambur",
+  keywords: [
+    "mariam corner",
+    "mariam crochet jewelry",
+    "maryna kambur",
+    "марина камбур",
+    "вязані прикраси",
+    "українські дизайнери",
+    "вязаные украшения",
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export async function generateStaticParams() {
