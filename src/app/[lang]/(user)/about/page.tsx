f24f2930695e06286d2393f77671668938e363dd
Olya <ponomaryova.olya.y@gmail.com>
2024-07-29 19:51:53 +0200

    fix: delete mongodb-client-encryption dependency

--- src/app/[lang]/(user)/about/page.tsx
+++ src/app/[lang]/(user)/about/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import HeroImage from "public/images/hero.jpg";
+import HeroImage from "../../../../../public/images/hero.jpg";
 import LinkButton from "@/components/uikit/button";
 import { Locale } from "../../../../../i18n.config";
 import { getDictionary } from "@/lib/dictionary";
