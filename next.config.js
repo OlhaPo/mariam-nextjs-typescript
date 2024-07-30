75ded24b58ba5fe5e0737944c6b1ec7423274851
Olya <ponomaryova.olya.y@gmail.com>
2024-07-30 14:21:28 +0200

    fix: update next config file  to allow images from any https source.

--- next.config.js
+++ next.config.js
@@ -1,27 +1,35 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  // images: {
+  //   domains: [
+  //     "lh3.googleusercontent.com",
+  //     "www.mariamcorner.com.ua",
+  //     "www.dropbox.com",
+  //   ],
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "lh3.googleusercontent.com",
+  //       pathname: "/a/**",
+  //     },
+  //     {
+  //       protocol: "https",
+  //       hostname: "www.mariamcorner.com.ua",
+  //       pathname: "/**",
+  //     },
+  //     {
+  //       protocol: "https",
+  //       hostname: "www.dropbox.com",
+  //       pathname: "/scl/**",
+  //     },
+  //   ],
+  // },
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "www.mariamcorner.com.ua",
-      "www.dropbox.com",
-    ],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        pathname: "/a/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.mariamcorner.com.ua",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.dropbox.com",
-        pathname: "/scl/**",
+        hostname: "**",
       },
     ],
   },
