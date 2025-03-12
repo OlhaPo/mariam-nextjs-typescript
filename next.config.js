986516c7ec8542721bda8a9111d50bea963a8d8a
Olya <ponomaryova.olya.y@gmail.com>
2025-03-12 17:39:21 +0100

    fix: update robots.txt; fix redirect 308 for pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  trailingSlash: false, 
+  redirects() {
+    return [
+      {
+        source: "/en/",
+        destination: "/en",
+        permanent: true, 
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
